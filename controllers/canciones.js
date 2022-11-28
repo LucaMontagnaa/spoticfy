6a4d453327a07dec494591b275b19b34c89d0293
LucaMontagnaa <101721205+LucaMontagnaa@users.noreply.github.com>
2022-11-28 11:26:47 -0300

    canciones

--- controllers/canciones.js
+++ controllers/canciones.js
@@ -50,6 +50,17 @@ const getCancion = (req, res) => {
             "reproducciones": "Reproducciones de la canción"
         }
     */
+    
+    let id = req.params.id;
+    const Selector = "SELECT canciones.id, canciones.nombre, artistas.nombre AS nombre_artista, albumes.nombre AS nombre_album, canciones.duracion, canciones.reproducciones FROM albumes JOIN artistas JOIN canciones WHERE albumes.id = ?";
+    conn.query(Selector, [id], (err, rows) => {
+        if (err) {
+            console.error("Hubo un error al consultar por: " + err);
+            return;
+        }
+        res.json(rows[0]);
+    });
+    
 
 
 };
@@ -66,6 +77,17 @@ const createCancion = (req, res) => {
         }
     */
     // (Reproducciones se inicializa en 0)
+
+    const { nombre, album, duracion } = req.body;
+    const Select = "INSERT INTO canciones (nombre,album,duracion) VALUES (?,?,?)";
+
+    conn.query(Select, [nombre, album, duracion], (err, rows) => {
+        if (err) {
+            console.error("Hubo un error al consultar por: " + err);
+            return;
+        }
+        res.sendStatus(100);
+    });
 };
 
 const updateCancion = (req, res) => {
@@ -80,16 +102,51 @@ const updateCancion = (req, res) => {
         }
     */
     // (Reproducciones no se puede modificar con esta consulta)
+
+    const id = req.params.id;
+    const { nombre, album, duracion } = req.body;
+    const Seleccionar = "UPDATE canciones SET nombre=?, album=?, duracion=? WHERE id=?";
+
+    conn.query(Seleccionar,[nombre, album, duracion, id],(err, rows) => {
+        if (err) {
+            console.error("Hubo un error al consultar por: " + err);
+            return;
+        }
+        res.sendStatus(100);
+    });
 };
 
 const deleteCancion = (req, res) => {
     // Completar con la consulta que elimina una canción
     // Recordar que los parámetros de una consulta DELETE se encuentran en req.params
+
+    const id = req.params.id;
+    const seleccionar = "DELETE FROM canciones WHERE id=?";
+
+    conn.query(seleccionar, [id], (err, rows) => {
+        if (err) {
+            console.error("Hubo un error al consultar por: " + err);
+            return;
+        }
+        res.sendStatus(100);
+    });
+
 };
 
 const reproducirCancion = (req, res) => {
     // Completar con la consulta que aumenta las reproducciones de una canción
     // En este caso es una consulta PUT, pero no recibe ningún parámetro en el body, solo en los params
+
+    const id = req.params.id;
+    const selection = "UPDATE canciones SET reproducciones= reproducciones + 1 WHERE id=?";
+
+    conn.query(selection, [id], (err, rows) => {
+        if (err) {
+            console.error("Hubo un error al consultar por: " + err);
+            return;
+        }
+        res.sendStatus(100);
+    });
 };
 
 module.exports = {
